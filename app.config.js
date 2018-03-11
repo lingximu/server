cedd4ab711cff61dd47db7fdc0e513fb2a5d053b
chenzhongchen <644522083@163.com>
2018-03-11 13:34:46 +0800

    :sparkles: update pm2 start config

---
+++ app.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name   : "app",
+    script : "./cli.js",
+    watch  : true,
+    args: "app",
+    env: {
+      "NODE_ENV": "production",
+      "PORT": "80"
+    },
+  }]
+}
