0e0a43aab857b097aad4a7c10701ef48d26bdfe3
chenzhongchen <644522083@163.com>
2018-03-14 23:16:38 +0800

    :sparkles: 反向代理静态资源

--- src/server/app.js
+++ src/server/app.js
@@ -2,23 +2,23 @@ var Koa = require('koa');
 var Router = require('koa-router');
 var path = require('path')
 const serve = require('koa-static');
+const router = require('./routers')
 
 var app = new Koa();
-var router = new Router();
+// var router = new Router();
+
 
 module.exports = (options) => {
 
-    const root = path.join(process.cwd(), '../lingximu.github.io/')
-    app.use(serve(root))
 
+  const root = path.join(process.cwd(), '../lingximu.github.io/')
+  app.use(serve(root))
 
-    router.get('/**', (ctx, next) => {
-      ctx.body = "404"
-      ctx.status = 404;
-    });
+  app.use(router.routes())
 
-    app
-      .use(router.routes())
-
+  router.get('/**', (ctx, next) => {
+    ctx.body = "404"
+    ctx.status = 404;
+  });
     return app;
 }
