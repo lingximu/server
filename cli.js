945873e1807b7d68cdf06006c0db3c2160b9146e
chenzhongchen <644522083@163.com>
2018-05-19 19:30:02 +0800

    :sparkles: Add babel to use es6 syntax
    syntax / debug about babel / cli.js add command dev and online

--- cli.js
+++ cli.js
@@ -1,12 +1,16 @@
 const program = require('commander');
 const pkg = require('./package.json');
+const shell = require('shelljs');
+const debug = require('debug')('server:cli')
 
 program
     .version(pkg.version)
-    .command('app')
-    .description('start app server')
+    .command('dev')
+    .description('start app server in develop')
     .option('-p, --port <port>', 'port (or $PORT)', Number, process.env.PORT || 8000)
     .action((options) => {
+        debug('action debug active')
+        console.log(program)
         const app = require('./src/server/app')({
             root: __dirname,
             port: options.port,
@@ -15,6 +19,29 @@ program
         app.listen(options.port, (err) => {
             console.info(`Server listen on ${options.port}`);
         });
-    });
+    })
+
+program
+    .version(pkg.version)
+    .command('online')
+        .description('start app server in online')
+        .option('-p, --port <port>', 'port (or $PORT)', Number, process.env.PORT || 8000)
+        .option('--no-build', 'can not exec npm build')
+        .action((options) => {
+            debug('action online active')
+            debug('options.build %o',options.build)
+            if(options.build){
+                shell.rm('-rf','./dist')
+                shell.exec('npm run build')
+            }
+            const app = require('./dist/server/app')({
+                root: __dirname,
+                port: options.port,
+                type: 'app',
+            });
+            app.listen(options.port, (err) => {
+                console.info(`Server listen on ${options.port}`);
+            });
+        });
 
 program.parse(process.argv);
