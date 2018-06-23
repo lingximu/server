129028dd8d44072645522b31793fa1340f868b2f
chenzhongchen <644522083@163.com>
2018-06-23 11:48:39 +0800

    :sparkles: 增加 db 配置

--- src/config/default.js
+++ src/config/default.js
@@ -1,15 +1,21 @@
-require('dotenv').config()
-const path = require('path')
+require('dotenv').config();
+const path = require('path');
 
 module.exports = {
-    private:{
-        name: process.env.private_name,
-        pass: process.env.private_pass
-    },
-    proxyParams:{
-        name: undefined,
-        target: undefined,
-        Referer: undefined
-    },
-    staticRootDir: path.join(process.cwd(), '../lingximu.github.io/')
-}
+  db: {
+    host: process.env.db_host,
+    database: process.env.db_database,
+    user: process.env.db_user,
+    password: process.env.db_password
+  },
+  private: {
+    name: process.env.private_name,
+    pass: process.env.private_pass
+  },
+  proxyParams: {
+    name: undefined,
+    target: undefined,
+    Referer: undefined
+  },
+  staticRootDir: path.join(process.cwd(), '../lingximu.github.io/')
+};
--- src/server/model/db.js
+++ src/server/model/db.js
@@ -8,28 +8,28 @@ const config = require('config');
 const logger = require('../..server/utils/logger');
 
 const log = logger.createLogger('myRn:src:server:model:db');
-const db = config.db;
+const dbConfig = config.db;
 // db
 const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: db.host,
-        database: db.database,
-        user: db.user,
-        password: db.password,
-        charset: 'utf8',
-    },
-    pool: {
-        min: 0,
-        max: db.poolSize || 10,
-    },
-    debug: true,
+  client: 'mysql2',
+  connection: {
+    host: dbConfig.host,
+    database: dbConfig.database,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    charset: 'utf8'
+  },
+  pool: {
+    min: 0,
+    max: dbConfig.poolSize || 10
+  },
+  debug: true
 });
 
-log.info(`数据库连接：connect to ${db.host}#${db.database}`);
+log.info(`数据库连接：connect to ${dbConfig.host}#${dbConfig.database}`);
 
 const db = require('bookshelf')(knex);
 db.plugin('pagination');
 db.plugin('registry');
 
-module.exports = db
+module.exports = db;
